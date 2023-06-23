1a67665a52f11f5a661d074a7e7a49240bd87973
zhangxingling2 <2350486903@qq.com>
2023-06-23 17:23:09 +0800

    mockgen生成proxy

--- micro/rpc/client_test.go
+++ micro/rpc/client_test.go
@@ -3,6 +3,7 @@ package rpc
 import (
 	"context"
 	"errors"
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -11,26 +12,42 @@ func TestSetFuncField(t *testing.T) {
 	testCases := []struct {
 		name    string
 		service Service
+		mock    func(ctrl *gomock.Controller) Proxy
 		wantErr error
 	}{
 		{
 			name:    "nil",
 			service: nil,
+			mock: func(ctrl *gomock.Controller) Proxy {
+				return NewMockProxy(ctrl)
+			},
 			wantErr: errors.New("服务是空服务"),
 		},
 		{
 			name:    "no pointer",
 			service: UserService{},
+			mock: func(ctrl *gomock.Controller) Proxy {
+				return NewMockProxy(ctrl)
+			},
 			wantErr: errors.New("只接受一级结构体指针"),
 		},
 		{
-			name:    "user service ",
+			name: "user service ",
+			mock: func(ctrl *gomock.Controller) Proxy {
+				p := NewMockProxy(ctrl)
+				p.EXPECT().Invoke(gomock.Any(), gomock.Any()).Return(&Response{}, nil)
+				return p
+			},
 			service: &UserService{},
 		},
 	}
 	for _, tc := range testCases {
+
 		t.Run(tc.name, func(t *testing.T) {
-			err := setFuncField(tc.service)
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			err := setFuncField(tc.service, tc.mock(ctrl))
 			assert.Equal(t, tc.wantErr, err)
 			if err != nil {
 				return
